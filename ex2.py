9249206f6c32b12be969b56c8a38850b5908737f
davidGBbr <davidgomesbrigido@gmail.com>
2024-04-18 17:49:06 -0300

    exercise 2

---
+++ ex2.py
@@ -0,0 +1,16 @@
+name = 'Cleber'
+age = 18
+height = 1.70
+is_admin = False
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_admin))
+
+valor_compras = 197.90
+desconto = valor_compras * 0.05
+quantidade_items = 5
+
+print("Valor total das compras com desconto:" + str(valor_compras - desconto))
+print("O custo médio por item foi de:" + str(valor_compras / 5))
